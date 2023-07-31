f076cb9f765be2774bc30e66dd8aa5865115cc6c
Adrian <83014095+AdrianKotyra@users.noreply.github.com>
2023-07-31 11:40:27 +0100

    Add files via upload

---
+++ CMS project/admin/update_categories.php
@@ -0,0 +1,9 @@
+<?php
+    if(isset($_POST["update"])) {
+        $cat_id = $_GET["update_cat"];
+        $the_cat_title_update = $_POST["cat_title_update"];
+        $query2 = "UPDATE categories SET category_title = '$the_cat_title_update' WHERE category_id='$cat_id'";
+        $update_category2 = mysqli_query($connection, $query2);
+    }
+
+?>
