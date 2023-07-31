f74b5bb475e1918b69e0570f52e91710437f537b
Adrian Kotyra <adriankotyra@yahoo.com>
2023-07-31 12:21:15 +0100

    are you

---
+++ admin/includes/admin_header.php
@@ -0,0 +1,26 @@
+<?php ob_start(); ?>
+
+
+<?php include "../includes/head.php"?>
+<?php include "functions.php"?>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>CMS PROJECT</title>
+
+    <link rel="stylesheet" href="../../css/stylesheet.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/sb-admin.css" rel="stylesheet">
+
+
+
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <?php include "admin_footer.php" ?>
+
+</head>
